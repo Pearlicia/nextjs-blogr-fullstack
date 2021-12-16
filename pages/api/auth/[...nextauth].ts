61deb58dfe3f796dd2de584fddb49088eec6815a
Felicia Ebikon <feliciaebikon@gmail.com>
2021-12-16 14:04:59 +0100

    fix github provider error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,43 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import Providers from 'next-auth/providers/github';
 import Adapters from 'next-auth/adapters';
 import prisma from '../../../lib/prisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { getProviders, providers } from 'next-auth/client';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
 const options = {
   providers: [
-    Providers.GitHub({
+    Providers({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  adapter: Adapters.Prisma.Adapter({ prisma }),
+  adapter: PrismaAdapter(prisma),
+  // adapter: Adapters.Prisma.Adapter({ prisma }),
   secret: process.env.SECRET,
 };
+
+// import NextAuth from "next-auth"
+// import Providers from "next-auth/providers"
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
+
+// const prisma = new PrismaClient()
+
+// export default NextAuth({
+//     // Configure one or more authentication providers
+//     providers: [
+//         Providers.Github ({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//     ],
+//     adapter: PrismaAdapter(prisma),
+// })
+
+
